0afb2dfc1c2381817debec18a912216b973b2aa6
iarml-workshop <iarml.workshop@gmail.com>
2025-04-08 20:28:13 +0200

    airml now

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Eriana
+# airml
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.10.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eriana'`, () => {
+  it(`should have as title 'airml'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('eriana');
+    expect(app.title).toEqual('airml');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('eriana app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('airml app is running!');
   });
 });
